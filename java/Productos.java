4101a9fbf8288011cbb59d9c6b210706a37e3071
camilodiaz232 <camiloadiaz232@gmail.com>
2024-02-18 11:16:43 -0500

    Initial commit

---
+++ java/Productos.java
@@ -0,0 +1,19 @@
+package java;
+
+public class Productos {
+
+	private int codigo;
+
+	private String nombreProducto;
+
+	private String tipoProducto;
+
+	private String fechaExpiracion;
+
+	private String nombreFabricante;
+
+	private int cantidadInventario;
+
+	private Double precioUnitario;
+
+}
---
+++ java/Tienda.java
@@ -0,0 +1,17 @@
+package java;
+
+public class Tienda {
+
+	private String nombre;
+
+	private String direccion;
+
+	private int telefono;
+
+	private Productos productos;
+
+	public void almacenarProducto() {
+
+	}
+
+}
